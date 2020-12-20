d621301d4888ec71118f78b3fc01c93beb286248
Samuel Melanson <samuelmelanson@live.com>
2020-12-20 13:57:34 -0500

    testing email schedule based on heroku timezone

--- scheduler/index.js
+++ scheduler/index.js
@@ -4,8 +4,8 @@ const cronRule = new schedule.RecurrenceRule();
 // Heroku server is at GMT + 00:00
 // cronRule.hour = 12; // local time is -5:00, so this should go off at 7:15
 // cronRule.minute = 15;
-cronRule.hour = 18; 
-cronRule.minute = 54;
+cronRule.hour = 19; 
+cronRule.minute = 5;
 
 module.exports = {
   cronRule,
