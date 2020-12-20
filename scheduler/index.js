870583e93ac76a0d1bea71c731900ae316e7992a
Samuel Melanson <samuelmelanson@live.com>
2020-12-20 14:13:11 -0500

    time set for 7:31 AM local

--- scheduler/index.js
+++ scheduler/index.js
@@ -2,10 +2,8 @@ const schedule = require('node-schedule');
 
 const cronRule = new schedule.RecurrenceRule();
 // Heroku server is at GMT + 00:00
-// cronRule.hour = 12; // local time is -5:00, so this should go off at 7:15
-// cronRule.minute = 15;
-cronRule.hour = 19; 
-cronRule.minute = 5;
+cronRule.hour = 12; // local time is -5:00, so this should go off at 7:15
+cronRule.minute = 31;
 
 module.exports = {
   cronRule,
