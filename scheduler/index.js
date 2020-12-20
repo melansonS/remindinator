3ace736e6664de82e0d3aedf9e0f2a73b3bc35ce
Samuel Melanson <samuelmelanson@live.com>
2020-12-20 13:47:35 -0500

    testing automated emails at cron time

--- scheduler/index.js
+++ scheduler/index.js
@@ -2,8 +2,10 @@ const schedule = require('node-schedule');
 
 const cronRule = new schedule.RecurrenceRule();
 // Heroku server is at GMT + 00:00
-cronRule.hour = 12; // local time is -5:00, so this should go off at 7:15
-cronRule.minute = 15;
+// cronRule.hour = 12; // local time is -5:00, so this should go off at 7:15
+// cronRule.minute = 15;
+cronRule.hour = 18; 
+cronRule.minute = 49;
 
 module.exports = {
   cronRule,
