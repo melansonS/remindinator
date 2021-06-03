30cf346c175f72ffc05690fb895c98c19cc9805d
Samuel Melanson <samuelmelanson@live.com>
2021-06-03 11:33:48 -0400

    auto login in a useCallback :ice:

--- client/src/Container.jsx
+++ client/src/Container.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Redirect, Route, useHistory, useLocation } from 'react-router-dom';
-import Dashboard from './views/Dashboard';
-import Header from './components/Header';
-import Login from './views/Login';
-import Signup from './views/Signup';
+import React, { useEffect, useState, useCallback } from "react";
+import { Redirect, Route, useHistory, useLocation } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import Header from "./components/Header";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
 
 const Container = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,33 +14,42 @@ const Container = () => {
     setIsLoggedIn(value);
   };
 
-  const autoLogin = async () => {
-    const response = await fetch('/api/v1/auto-login', {
-      credentials: 'include',
+  const autoLogin = useCallback(async () => {
+    const response = await fetch("/api/v1/auto-login", {
+      credentials: "include",
     });
     const body = await response.json();
     setIsLoggedIn(body.success);
     if (body.success) {
       // navigate directly to the Dashboard
-      history.push('/dashboard');
-    } else if (location.pathname !== '/login') {
+      history.push("/dashboard");
+    } else if (location.pathname !== "/login") {
       // navigate back to the Signup Page
-      history.push('/');
+      history.push("/");
     }
-  };
+  }, [history, location.pathname]);
+
   useEffect(() => {
     autoLogin();
-  });
+  }, [autoLogin]);
 
   return (
     <div className="container">
       <Header handleLoggedUpdate={handleLoggedUpdate} isLoggedIn={isLoggedIn} />
       <div className="main-container">
-      <Route exact path="/">
-          {isLoggedIn ? <Redirect to="/dashboard" /> : <Signup handleLoggedUpdate={handleLoggedUpdate} />}
+        <Route exact path="/">
+          {isLoggedIn ? (
+            <Redirect to="/dashboard" />
+          ) : (
+            <Signup handleLoggedUpdate={handleLoggedUpdate} />
+          )}
         </Route>
         <Route exact path="/login">
-          {isLoggedIn ? <Redirect to="/dashboard" /> : <Login handleLoggedUpdate={handleLoggedUpdate} />}
+          {isLoggedIn ? (
+            <Redirect to="/dashboard" />
+          ) : (
+            <Login handleLoggedUpdate={handleLoggedUpdate} />
+          )}
         </Route>
         <Route exact path="/dashboard">
           {isLoggedIn ? <Dashboard /> : <Redirect to="/dashboard" />}
