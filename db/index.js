53658e7fa2a88d1782e88871a28173a4f99d7d4b
Samuel Melanson <samuelmelanson@live.com>
2021-06-03 12:16:20 -0400

    testing heroku postgress connection

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const devConfig = {
   user: process.env.PGUSER,
@@ -10,9 +10,14 @@ const devConfig = {
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL, // from heroku
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
-const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig);
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = {
   query: (text, params, callback) => pool.query(text, params, callback),
